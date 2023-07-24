7950911fafe10b9cb665649a4c22e9d83c624d0f
prasadperugu-earth <137290200+prasadperugu-earth@users.noreply.github.com>
2023-07-24 21:21:23 +0530

    kafka added

--- src/main/java/com/prasad/scm/springapplication/SpringapplicationApplication.java
+++ src/main/java/com/prasad/scm/springapplication/SpringapplicationApplication.java
@@ -1,13 +1,20 @@
 package com.prasad.scm.springapplication;
 
+
+import com.prasad.scm.springapplication.kafka.producer.SendMessage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class SpringapplicationApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringapplicationApplication.class, args);
+    public static void main(String[] args) throws IOException {
+        ApplicationContext context =SpringApplication.run(SpringapplicationApplication.class, args);
+        SendMessage send =context.getBean(SendMessage.class);
+        send.sendMessageToTopic();
     }
 
 }
